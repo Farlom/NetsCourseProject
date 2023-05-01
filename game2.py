95b2205ae509d6a31a164eb58824a9f06432d1e3
Stepan Farlom <stepan.farlom@gmail.com>
2023-05-01 15:49:34 +0300

    установление подключения клиента с сервером

--- game2.py
+++ game2.py
@@ -3,6 +3,9 @@ from server import Server
 from client import Client
 import time
 from threading import Thread
+
+import socket
+
 choice = int(input("Клиент = 1, Сервер = 2: "))
 
 if choice == 1:
@@ -17,19 +20,19 @@ elif choice == 2:
     server = Server()
 
     def send_broadcast():
-        while True:
+        while server.client_ip is None:
             server.send_broadcast()
             time.sleep(5)
+        print('Подключение установлено')
 
     def get_client_ip():
-        while not server.get_client_ip():
-            server.get_client_ip()
+        server.get_client_ip()
         print(server.client_ip)
 
     thread_broad = Thread(target=send_broadcast)
-    # thread_client = Thread(target=get_client_ip)
+    thread_client = Thread(target=get_client_ip)
     thread_broad.start()
-    server.get_client_ip()
-    # print(server.client_ip)
-    # thread_client.start()
+    thread_client.start()
+
+
 
